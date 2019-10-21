a0219ff64d7a4bb50ed61e58512d50b544a409fa
jj1234321 <47927305+PHCodes@users.noreply.github.com>
2019-10-21 08:20:14 -0400

    Updates

---
+++ precache-manifest.64811d06e16b9149b51ab12289436469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c75de404cdbe4296396d446943a66d",
+    "url": "/basic-profile/index.html"
+  },
+  {
+    "revision": "3d5f2a89ee5a05982492",
+    "url": "/basic-profile/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d08ea1aaceeb21a422fe",
+    "url": "/basic-profile/static/js/2.36c8a57d.chunk.js"
+  },
+  {
+    "revision": "3d5f2a89ee5a05982492",
+    "url": "/basic-profile/static/js/main.5721e225.chunk.js"
+  },
+  {
+    "revision": "7fba86140e52476c40a5",
+    "url": "/basic-profile/static/js/runtime-main.b092fcab.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/basic-profile/static/media/logo.25bf045c.svg"
+  }
+]);
